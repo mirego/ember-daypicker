8d1f16c5d1feaef329be33b77a0a0953c530c2f5
Mathieu Cote <mcote@mirego.com>
2017-08-23 17:37:20 -0400

    Use moment i18n months and weeks in the daypicker
    Instead of hardcoding the strings in English, we use moment to fetch week day names and month names so that if moment is configured with another locale, it's free!

--- addon/utils/constants.js
+++ addon/utils/constants.js
@@ -1,16 +1,3 @@
 export default {
-  weekdays: {
-    "Sun": "Sunday",
-    "Mon": "Monday",
-    "Tue": "Tuesday",
-    "Wed": "Wednesday",
-    "Thu": "Thursday",
-    "Fri": "Friday",
-    "Sat": "Saturday"
-  },
-
-  months: ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"],
-
   defaultFormat: "MMM DD, YYYY"
 }
--- config/environment.js
+++ config/environment.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return { };
+  return {
+    moment: {
+      // To cherry-pick specific locale support into your application.
+      // Full list of locales: https://github.com/moment/moment/tree/2.10.3/locale
+      includeLocales: ['en', 'fr']
+    }
+  };
 };
