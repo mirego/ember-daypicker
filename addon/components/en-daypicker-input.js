0b09ef642a73008b80da9e53c8196a58d8aa7a9d
Benoit Ducharme <bducharme@mirego.com>
2017-10-02 13:44:53 -0400

    Add the possibility to disable the daypicker (#4)

--- addon/components/en-daypicker-input.js
+++ addon/components/en-daypicker-input.js
@@ -49,17 +49,21 @@ You passed in ${date}, which is invalid, so we're defaulting to today's date`)
 
   actions: {
     focus() {
-      run(() => {
-        this.set("isFocused", true)
-        this.attrs["on-focus"]
-      })
+      if (!this.get('disabled')) {
+        run(() => {
+          this.set("isFocused", true)
+          this.attrs["on-focus"]
+        })
+      }
     },
 
     didSelect(date) {
-      run(() => {
-        this.set("isFocused", false)
-        this.attrs["on-select"](date)
-      })
+      if (!this.get('disabled')) {
+        run(() => {
+          this.set("isFocused", false)
+          this.attrs["on-select"](date)
+        })
+      }
     }
   }
 })
